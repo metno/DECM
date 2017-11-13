2cfe6f90310087b784663cfc061c25e9eddeb986
kajsamp <kajsamp@met.no>
2017-11-13 12:39:16 +0100

    minor back-end changes

--- back-end/R/cdotools.R
+++ back-end/R/cdotools.R
@@ -2,7 +2,7 @@
 # Is this function affected by the error that Abdelkader discovered with -timmean? No.
 
 cdo.mean <- function(model.file,period=c(1981,2010),mask=NULL,seasonal=FALSE,
-                     monthly=FALSE,is.temp=TRUE,verbose=FALSE) {
+                     monthly=FALSE,is.temp=TRUE,out.file=NULL,verbose=FALSE) {
   
   commands <- c("-fldmean","-timmean","-selyear")
   input <- c("","",paste(period,collapse="/"))
@@ -17,7 +17,13 @@ cdo.mean <- function(model.file,period=c(1981,2010),mask=NULL,seasonal=FALSE,
     commands <- replace(commands,commands=="-timmean","-yseasmean")
   }
   
-  out.file <- "tmp.nc"
+  if(is.null(out.file)) {
+    out.file <- "tmp.nc"
+    save.file <- FALSE
+  } else {
+    save.file <- TRUE
+  }
+  
   cdo.command(commands,input,model.file,out.file)
   
   command <- ("output")
@@ -33,7 +39,7 @@ cdo.mean <- function(model.file,period=c(1981,2010),mask=NULL,seasonal=FALSE,
   } 
   # If applying to e.g. slp data, set is.temp to FALSE to skip this correction:
   if(out>200 & is.temp) out <- out-273.15 
-  system(paste("rm",out.file,sep=" "))
+  if(!save.file) system(paste("rm",out.file,sep=" "))
   invisible(out)
 }
 
--- back-end/R/regions.R
+++ back-end/R/regions.R
@@ -22,7 +22,7 @@ getPolCoords <- function(region,shape=NULL,destfile="coords.txt"){
 }
 
 #apply mask to a zoo object by setting values outside the mask to NA
-mask.zoo <- function(zoo.object,mask){
+mask.zoo <- function(zoo.object,mask) {
   mask <- flip(mask,direction='y')
   zoo.object[,which(is.na(getValues(mask)))] <- NA
   return(zoo.object)

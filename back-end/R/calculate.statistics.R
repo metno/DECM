ea9f824cb041c2b31b50a0a16743e802118dee59
kajsamp <kajsamp@met.no>
2017-11-14 14:42:51 +0100

    updated functions to calculate CMIP5 and CORDEX statistics

--- back-end/R/calculate.statistics.R
+++ back-end/R/calculate.statistics.R
@@ -119,6 +119,60 @@ calculate.statistics.cmip <- function(reference="era", period=c(1981,2010), vari
   return(store)
 }
 
+calculate.statistics.cordex <- function(reference="era", period=c(1981,2010), variable="tas", 
+                                        nfiles=5, continue=TRUE, verbose=FALSE, mask="coords.txt"){
+  
+  region <- read.csv(find.file("RegionSpecifications.csv"))
+  region.id <- as.character(region$Code)
+  if(max(period)>2015) reference <- NULL
+  if(!is.null(reference)) {
+    store.file <- paste("statistics.cordex", reference, variable, paste(period, collapse="-"), "rda", sep=".")
+  } else {
+    store.file <- paste("statistics.cordex", variable, paste(period, collapse="-"), "rda", sep=".")
+  }
+  
+  store <- list()
+  if(file.exists(store.file)) load(store.file)
+  
+  if(!is.null(reference)) {
+    ref.file <- getReference(reference,variable)
+    reference.raster <- raster(ref.file)
+    
+    store.name <- paste(reference,variable,sep=".")
+    store[[store.name]]$spatial.sd <- c(cdo.spatSd(ref.file,period), 
+                                        cdo.spatSd(ref.file,period,monthly=TRUE))
+    store[[store.name]]$mean <- c(cdo.mean(ref.file,period), 
+                                  cdo.mean(ref.file,period,monthly=TRUE))
+  }
+  
+  ngcm <- length(cordex.urls(varid=variable))
+  start <- 1
+  if(continue && file.exists(store.file))
+    start <- as.numeric(tail(sub('.*\\.', '', names(store), perl=TRUE),n=1))+1
+  if(nfiles=="all") {
+    end <- ngcm
+  } else {
+    end <- min(start+nfiles-1,ngcm) 
+  }
+  
+  for(i in start:end){
+    X <- getRCMs(select=i,varid=variable)
+    gcm.file <- X[[1]]$filename
+    store.name <- paste("rcm",i,sep=".")
+    store[[store.name]]$spatial.sd <- c(cdo.spatSd(gcm.file,period),
+                                        cdo.spatSd(gcm.file,period,monthly=TRUE))
+    store[[store.name]]$mean <- c(cdo.mean(gcm.file,period),
+                                  cdo.mean(gcm.file,period,monthly=T))
+    if(!is.null(reference)) store[[store.name]]$corr <- 
+      c(cdo.gridcor(gcm.file,ref.file,period),
+        cdo.gridcor(gcm.file,ref.file,period,monthly=TRUE))
+    save(file=store.file,store)
+    gc()
+    if(i==ngcm) return(store)
+  }
+  return(store)
+}
+
 ## Calculate weights for the weighted average of rms, assuming monthly time step.
 calculate.mon.weights <- function(lon,lat) {
   
@@ -227,6 +281,90 @@ calculate.rmse.cmip <- function(reference="era", period=c(1981,2010), variable="
   median.rms <- median(unlist(lapply(store, "[","rms")))  
   for(i in start:end){
     store.name <- paste("gcm",i,sep=".")
+    store[[store.name]]$e <- (store[[store.name]]$rms-median.rms)/median.rms
+    for(region in levels(shape$LAB)){
+      median.rms <- median(unlist(lapply(lapply(store, "[",region),function(x) x[[region]]$rms)))
+      store[[store.name]][[region]]$e <- (store[[store.name]][[region]]$rms-median.rms)/median.rms
+    }
+  }
+  save(file=store.file, store)
+  file.remove(ref.mon.file)
+  invisible(store)
+}
+
+calculate.rmse.cordex <- function(reference="era", period=c(1981,2010), variable="tas", nfiles=4,
+                                  continue=TRUE, verbose=FALSE, path=NULL) {
+  if(verbose) print("calculate.rmse.cordex")
+
+  store <- list()
+  store.file <- paste("statistics.cordex", reference, variable, paste(period, collapse="-"),
+                      "rda", sep=".")
+  if(is.character(find.file(store.file)[1])) store.file <- find.file(store.file)[1]
+  if(file.exists(store.file)) load(store.file)
+  
+  ## Pre-process reference file if necessary
+  ref.file <- getReference(reference,variable)
+  if(is.null(path)) {
+    path <- dirname(ref.file)
+  } else if (!dir.exists(path)) {
+    path <- getwd()
+  }
+  
+  ref.mulc <- paste(reference,"mulc",variable,"nc",sep=".")
+  if(!is.character(find.file(ref.mulc)[1])) ref.mulc <- file.path(path,ref.mulc)
+  ref.mon.file <- paste(reference,"monmean",variable,"nc",sep=".")
+  if(!is.character(find.file(ref.mon.file)[1])) ref.mon.file <- file.path(path,ref.mon.file)
+  
+  if(variable=="pr") {
+    if(!file.exists(ref.mulc)) cdo.command("mulc",1000,ref.file,ref.mulc)
+  } else {
+    if(!file.exists(ref.mulc)) ref.mulc <- ref.file
+  }
+  
+  if(!file.exists(ref.mon.file)) {
+    cdo.command(c("-ymonmean","-selyear"),c("",paste(period,collapse="/")),
+                infile=ref.mulc,outfile=ref.mon.file)
+  }
+  ref <- coredata(retrieve(ref.mon.file))
+  
+  ## Calculate weights only once
+  r <- raster(ref)#.mon.file)
+  ## KMP 2017-11-13: these weights don't fit the gcm and ref on line 211
+  ## and xFromCol/yFromRow(r) doesn't match longitude/latitude(ref)
+  #lon <- xFromCol(r)
+  #lat <- yFromRow(r)
+  lon <- longitude(ref)
+  lat <- latitude(ref)
+  weights <- calculate.mon.weights(lon,lat)
+  
+  ## Check which files are processed
+  ngcm <- length(cordex.urls(varid=variable))
+  start <- 1
+  if(continue && file.exists(store.file)) {
+    start <- as.numeric(tail(sub('.*\\.', '', names(store), perl=TRUE), n=1)) + 1
+  }
+  if(nfiles=="all") {
+    end <- ngcm
+  } else {
+    end <- min(start + nfiles - 1, ngcm) 
+  }
+  
+  for(i in start:end) {
+    store.name <- paste("cm",i,sep=".")
+    gcm.file <- file.path(path,paste("CM",i,".",variable,".nc",sep=""))
+    if(!file.exists(gcm.file)) getGCMs(i, varid=variable, destfile=file.path(path,gcm.file))
+    gcm.mon.file <- file.path(path,"cm.monmean.nc")
+    cdo.command(c("-ymonmean","-selyear"),c("",paste(period,collapse="/")),
+                infile=gcm.file,outfile=gcm.mon.file)
+    gcm <- coredata(retrieve(gcm.mon.file))
+    dim(gcm) <- dim(ref) <- c(12,length(longitude(gcm)),length(latitude(gcm)))
+    store[[store.name]]$rms <- sqrt(sum(weights*(gcm-ref)^2)/sum(weights))
+    file.remove(gcm.mon.file)
+  }
+  
+  median.rms <- median(unlist(lapply(store, "[","rms")))  
+  for(i in start:end){
+    store.name <- paste("cm",i,sep=".")
     store[[store.name]]$e <- (store[[store.name]]$rms-median.rms)/median.rms
     for(region in levels(shape$LAB)){
       median.rms <- median(unlist(lapply(lapply(store, "[",region),function(x) x[[region]]$rms)))

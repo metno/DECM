ef3af34de889696073d65b1a7682ab14a5368d89
kajsamp <kajsamp@met.no>
2018-12-07 11:04:08 +0100

    problem with function thredds.urls

--- back-end/R/thredds.urls.R
+++
@@ -1,9 +0,0 @@
-thredds.urls <- function(url.rel="raw/tas",pattern=".*EUR-11.*.nc",select=NULL,
-  url.base="http://thredds.met.no/thredds/catalog/postclim/data/CORDEX-EUR11",
-  url.download="http://thredds.met.no/thredds/dodsC/postclim/data/CORDEX-EUR11",
-  verbose=FALSE,...) {
-
-  url <- url.base
-  if(verbose) print("thredds.urls")
-  
-}

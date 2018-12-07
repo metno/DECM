6572c33568d5132145d992a695ee8848a852ba6f
kajsamp <kajsamp@met.no>
2018-12-07 11:07:40 +0100

    removing unused function testrank

--- back-end/R/testrank.R
+++
@@ -1,7 +0,0 @@
-testrank <- function(x,m,N,d1) {
-  ## Find the rank number of m observations
-  ## Need to unwrap the dimensions
-  dim(x) <- c(N,d1)
-  number <- apply(x,1,function(x) order(x)[1:m])
-  return(c(number))
-}
